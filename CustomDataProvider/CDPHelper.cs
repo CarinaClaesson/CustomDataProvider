c42934017dcf3a0db11d9dcfcc332aedb17bc3bb
Carina Claesson <carina@carinaclaesson.com>
2020-01-17 09:31:47 +0100

    Removed the parts which transformed string to guid and guid to sting

--- CustomDataProvider/CDPHelper.cs
+++ CustomDataProvider/CDPHelper.cs
@@ -1,57 +1,8 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace CustomDataProvider
 {
-    static class CDPHelper {
-
-        /// <summary>
-        /// Initializes a TripleDESCryptoServiceProvider
-        /// </summary>
-        /// <returns>an instance of TripleDESCryptoServiceProvider</returns>
-        public static TripleDES GetCryptoServiceProvider()
-        {
-            var md5 = new MD5CryptoServiceProvider();
-            var des = new TripleDESCryptoServiceProvider();
-            var desKey = md5.ComputeHash(Encoding.UTF8.GetBytes("fjdoe85d"));
-            des.Key = desKey;
-            des.IV = new byte[des.BlockSize / 8];
-            des.Padding = PaddingMode.PKCS7;
-            des.Mode = CipherMode.ECB;
-            return des;
-        }
-
-        /// <summary>
-        /// Convert string to Guid. Please note! Works for strings less than 16 bytes only.
-        /// </summary>
-        /// <param name="value">the string value</param>
-        /// <returns>the Guid value</returns>
-        public static Guid StringToGuid(string value)
-        {
-            var des = GetCryptoServiceProvider();
-            var ct = des.CreateEncryptor();
-            var input = Encoding.UTF8.GetBytes(value);
-            var output = ct.TransformFinalBlock(input, 0, input.Length);
-            return new Guid(output);
-        }
-
-        /// <summary>
-        /// Convert Guid to string
-        /// </summary>
-        /// <param name="value">the Guid value</param>
-        /// <returns>the string value</returns>
-        public static string GuidToString(Guid value)
-        {
-            var des = GetCryptoServiceProvider();
-            var guidInBytes = value.ToByteArray();
-            var str = Convert.ToBase64String(guidInBytes);
-            var ctd = des.CreateDecryptor();
-            var input = Convert.FromBase64String(str);
-            var output = ctd.TransformFinalBlock(input, 0, input.Length);
-            return Encoding.UTF8.GetString(output);
-        }
-                      
+    static class CDPHelper {                 
         /// <summary>
         /// Convert int to Guid
         /// </summary>
